1d15c557cf5b19d9046d512baa861880dbe69499
Krishna Sharma <krishnasharma.delhi1@gmail.com>
2022-02-03 12:28:29 +0530

    Header changed

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 schoollist = [{"School.Name":"Bhola Nath Nagar-SBV (Babu Ram)","School.id":1001001}]
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8000, , Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
     next();
 });
 app.get('/', async(req, res) => {
