4b7033116d988c994d27ea515bf40f7022b69934
Krishna Sharma <krishnasharma.delhi1@gmail.com>
2022-02-08 12:29:01 +0530

    req url changed

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 schoollist = [{"name":"Bhola Nath Nagar-SBV (Babu Ram)","id":1001001}]
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001/");
     next();
 });
 app.get('/', async(req, res) => {
